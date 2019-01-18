3bdaf0beaf14e3310151a1cef9949c6d8e3e93d8
Anthony CASANAVE <anthony.casanave@gmail.com>
2019-01-18 11:19:27 +0100

    Adding config file with database infos

---
+++ config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+	$host= 'localhost'; //Ou adresse IP du server
+	$dbname = 'dht11';
+	$username= 'dht11';
+	$password = 'Dht11!Dta1$2019';
+
+    $bdd= new PDO("mysql:host=".$host.";dbname=".$dbname, $username, $password);
+
+
+
+
+
+
+
+
+
+
